6ae893a8cb80877fb9fe71f9fba7e5a06445646c
zrxu2006 <zrxu2006@hotmail.com>
2016-03-13 15:00:05 +0800

    POC追加

---
+++ POC/EsbLog.Poc.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using EsbLog.Poc.Consumers;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsbLog.Poc.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+                {
+                    var host = cfg.Host(new Uri("rabbitmq://localhost/"), h =>
+                        {
+                            h.Username("guest");
+                            h.Password("guest");
+                        });
+
+                    cfg.ReceiveEndpoint(host, "customer_update_queue", e =>
+                        {
+                            e.Consumer<UpdateCustomerConsumer>();
+                        });
+                });
+        }
+    }
+}
---
+++ POC/EsbLog.Poc.Contracts/IUpdateCustomerAddress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsbLog.Poc.Contract
+{
+    public interface IUpdateCustomerAddress
+    {
+        Guid CommandId { get; }
+        DateTime Timestamp { get; }
+        string CustomerId { get; }
+        string HouseNumber { get; }
+        string Street { get; }
+        string City { get; }
+        string State { get; }
+        string PostalCode { get; }
+    }
+}
