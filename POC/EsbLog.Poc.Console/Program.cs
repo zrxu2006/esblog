02cce80e3fcd87126aaf4f8bc7c5799635b55213
zrxu2006 <zrxu2006@hotmail.com>
2016-05-02 21:08:52 +0800

    poc

--- POC/EsbLog.Poc.Console/Program.cs
+++ POC/EsbLog.Poc.Console/Program.cs
@@ -1,10 +1,12 @@
 ﻿using EsbLog.Poc.Consumers;
+using EsbLog.Poc.Contracts;
 using MassTransit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SysConsole = System.Console;
 
 namespace EsbLog.Poc.Console
 {
@@ -12,19 +14,48 @@ namespace EsbLog.Poc.Console
     {
         static void Main(string[] args)
         {
-            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
-                {
-                    var host = cfg.Host(new Uri("rabbitmq://localhost/"), h =>
-                        {
-                            h.Username("guest");
-                            h.Password("guest");
-                        });
+            var busControl = ConfigureBus();
 
-                    cfg.ReceiveEndpoint(host, "customer_update_queue", e =>
-                        {
-                            e.Consumer<UpdateCustomerConsumer>();
-                        });
+            
+                busControl.Start();
+            
+                do
+                {
+
+                    SysConsole.WriteLine("ENter message (or quit to exit)");
+                    SysConsole.Write("> ");
+                    string value = SysConsole.ReadLine();
+
+                    if("quit".Equals(value,StringComparison.OrdinalIgnoreCase))
+                    {
+                        break;
+                    }
+
+                    busControl.Publish<IValueEntered>(new { Value = value });
+
+                } while (true);
+            
+        }
+
+        private static IBusControl ConfigureBus()
+        {
+            //return Bus.Factory.CreateUsingInMemory(cfg => 
+            //{ 
+            //    //cfg.
+            //});
+            return Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.Host(new Uri("rabbitmq://192.168.1.100/"), h =>
+                {
+                    h.Username("esblog");
+                    h.Password("esblog");
                 });
+
+                //cfg.ReceiveEndpoint(host, "customer_update_queue", e =>
+                //{
+                //    e.Consumer<UpdateCustomerConsumer>();
+                //});
+            });
         }
     }
 }
---
+++ POC/EsbLog.Poc.Servises/EsbLog.Poc.Services/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EsbLog.Poc.Contracts;
+using MassTransit;
+using Topshelf;
+using log4net;
+using System.IO;
+using log4net.Config;
+
+namespace EsbLog.Poc.Services
+{   
+    public class Program
+    {
+        static void Main(string[] args)
+        {            
+            HostFactory.Run(cfg => cfg.Service(x => new EventConsumerService()));
+        }
+    }
+
+    class EventConsumerService : ServiceControl
+    {
+        readonly ILog _log = LogManager.GetLogger(
+                                         typeof(EventConsumerService));
+        IBusControl _bus;
+        public bool Start(HostControl hostControl)
+        {
+            var logCfg = new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config");
+            XmlConfigurator.ConfigureAndWatch(logCfg);
+            _bus = ConfigureBus();
+            _bus.Start();
+            _log.Info("Service Started.");
+            return true;
+        }
+
+        private IBusControl ConfigureBus()
+        {
+            return Bus.Factory.CreateUsingRabbitMq(cfg =>
+                {
+                    var host = cfg.Host(new Uri("rabbitmq://192.168.1.100/"), h =>
+                    {
+                        h.Username("esblog");
+                        h.Password("esblog");
+                    });
+
+                    cfg.ReceiveEndpoint(host, "event_queue", e =>
+                    {
+                        e.Handler<IValueEntered>(context => 
+                        { 
+                            return Task.Run(() =>
+                            {
+                                _log.InfoFormat("value entered:{0}", context.Message.Value);
+                                Console.Out.WriteLine("new value:{0}", context.Message.Value);
+                            });
+                        });
+                    });
+                });
+            //return Bus.Factory.CreateUsingInMemory(cfg =>
+            //{
+            //    cfg.ReceiveEndpoint("event_queue", e =>
+            //    {
+            //        e.Handler<IValueEntered>(context =>
+            //        {
+            //            return Task.Run(() =>
+            //            {                           
+            //                _log.InfoFormat("value entered:{0}",context.Message.Value);
+            //                Console.Out.WriteLine("new value:{0}", context.Message.Value);
+            //            });
+            //        });
+
+            //    });
+            //});
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            _bus.Stop();
+
+            return true;
+        }
+    }
+}
