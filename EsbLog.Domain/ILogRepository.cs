942d25dde48abcebf19cbee5d5431429075c1451
zrxu2006@hotmail.com <zrxu2006@hotmail.com>
2016-06-03 07:06:36 +0800

    追加website

--- EsbLog.Domain/ILogRepository.cs
+++ EsbLog.Domain/ILogRepository.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EsbLog.Esb
+namespace EsbLog.Domain
 {
-    public class Class1
+    public interface ILogRepository
     {
+        int Save(Log entity);
+        Task<int> SaveAsync(Log entity);
     }
 }
