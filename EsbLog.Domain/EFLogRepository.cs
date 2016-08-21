ac7e1adc32961e1b41ab0779654fcfd1dafea8cc
zrxu2006@hotmail.com <zrxu2006@hotmail.com>
2016-08-21 17:36:47 +0800

    追加Domain

---
+++ EsbLog.Domain/EFLogRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsbLog.Domain
+{
+    public class EFLogRepository :ILogRepository
+    {
+        public EFLogRepository()
+        {
+        }
+
+        public Task<int> SaveAsync(Log entity)
+        {
+            return Task.Run(() => Save(entity));
+            //using(var db = new EsbLogContainer())
+            //{
+            //    db.LogSet.Add(log);
+            //    return db.SaveChangesAsync();
+            //}
+            
+        }
+
+        public int Save(Log entity)
+        {
+            using(var db = new EsbLogContainer())
+            {
+                db.LogSet.Add(entity);
+                return db.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ EsbLog.Esb/Consume/SaveLogConsumer.cs
@@ -0,0 +1,37 @@
+﻿using EsbLog.Domain;
+using EsbLog.Esb.Message;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsbLog.Esb.Consume
+{
+    /// <summary>
+    /// 保存日志Consumer
+    /// </summary>
+    public class SaveLogConsumer : IConsumer<ILogRequested>
+    {
+        //ILogRepository _repository;
+        //public SaveLogConsumer(ILogRepository rep)
+        //{
+        //    _repository = rep;
+        //}
+
+        public Task Consume(ConsumeContext<ILogRequested> context)
+        {
+            Log log = new Log();
+            log.AppId = context.Message.AppId;
+            log.Level = context.Message.LogLevel;
+            log.Ticks = context.Message.Ticks;
+            log.Content = context.Message.Content;
+
+            var repository = new EFLogRepository();
+
+            return Task.Run(() => repository.Save(log));
+            //return _repository.SaveAsync(log);
+        }
+    }
+}
