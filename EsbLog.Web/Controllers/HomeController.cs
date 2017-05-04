dd946c8c35bb2aa1961294d0ace6e804893f48a3
roy.xu <zrxu2006@hotmail.com>
2017-05-04 21:33:36 +0800

    追加platform

---
+++ EsbLog.Web/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EsbLog.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+	}
+}
---
+++ EsbLog.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EsbLog.Web.MvcApplication" Language="C#" %>
