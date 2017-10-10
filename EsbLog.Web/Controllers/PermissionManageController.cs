cb5b6bd000247dd280c7753a54acb8e05c6c358f
roy.xu <zrxu2006@hotmail.com>
2017-10-10 22:03:39 +0800

    应用管理Index使用 Matrix Layout

--- EsbLog.Web/Controllers/PermissionManageController.cs
+++ EsbLog.Web/Controllers/PermissionManageController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EsbLog.Domain.Platform;
+using EsbLog.Web.Models;
+using EsbLog.Web.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,33 @@ namespace EsbLog.Web.Controllers
 {
     public class PermissionManageController : Controller
     {
+        IAppManagerRepository _appRepo;
+        public PermissionManageController(IAppManagerRepository appRepository)
+        {
+            _appRepo = appRepository;
+        }
         //
         // GET: /Permission/
         public ActionResult Index()
+        {
+            App a = new App
+            {
+                AppId = 1,
+                Code = "APP01",
+                Name = "App-01",
+                PublicKey = Guid.NewGuid().ToString()
+            };
+
+
+            
+            ViewBag.AppList = _appRepo.FindAllApps();
+            ViewBag.AppList = new List<App>();
+            ViewBag.AppList.Add(a);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(PermissionQueryRequest queryRequest)
         {
             return View();
         }
